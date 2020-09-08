c6a891b08b5c6061405e8007d10b4077f69eea6b
Henrik Horeis <9387550+henrik432@users.noreply.github.com>
2020-09-08 15:12:19 +0200

    First step.

---
+++ src/main/kotlin/com/example/GithubController.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller()
+class GithubController {
+
+    @Get("/ping")
+    fun ping() = "pong"
+}
